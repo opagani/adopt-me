14f0ca1108cb2eb66f3dc2aac614405a899eac22
Oscar Pagani <opagani@zillowgroup.com>
2021-03-11 10:55:11 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Pet from "./Pet";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+//   ]);
+// };
+
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
